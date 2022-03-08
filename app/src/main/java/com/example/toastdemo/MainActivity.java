46903d9de38b4638819cdc47d02f43a06c40fb96
WIDYA <widylestari9@gmail.com>
2022-03-08 11:11:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/toastdemo/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.toastdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void buttonClicked(View view){
+        EditText editText = (EditText) findViewById(R.id.EditText);
+
+        Toast.makeText(this, "Hello" + editText.getText().toString(), Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
